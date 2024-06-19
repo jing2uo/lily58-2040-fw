f76f85a08fe827612492563c0a30d0830fbba3cf
bao.liu <bao.liu@grabtaxi.com>
2024-06-19 14:30:30 +0800

    add oled feature

--- keymaps/vial/rules.mk
+++ keymaps/vial/rules.mk
@@ -2,3 +2,13 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
 COMBO_ENABLE = yes
+OLED_ENABLE = yes
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
